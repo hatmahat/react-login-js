a607d5e2e73f6f691f5802e6ce2e0ec778ce88f2
hatmahat <hatmahat@gmail.com>
2023-03-25 17:33:35 +0700

    React Login Authentication with JWT Access, RefreshTokens, Cookies, and Axios

---
+++ src/components/Users.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import { useNavigate, useLocation } from "react-router-dom";
+
+export const Users = () => {
+    const [users, setUsers] = useState();
+    const axiosPrivate = useAxiosPrivate();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        let isMounted = true;
+        // cancellation tokens, is to cancel our
+        // request and we'll do that if the component unmounts
+        // cancel any pending request that is still out there if
+        // the component unmounts
+        const controller = new AbortController();
+
+        const getUsers = async () => {
+            try {
+                const response = await axiosPrivate.get("/users", {
+                    signal: controller.signal,
+                });
+                console.log("Users response", response.data);
+                isMounted && setUsers(response.data);
+            } catch (e) {
+                console.error("**Error in Users:", e);
+                navigate("/login", {
+                    state: { from: location },
+                    replace: true,
+                });
+            }
+        };
+
+        getUsers();
+
+        return () => {
+            // clean up functions runs as the component unmounts
+            isMounted = false;
+            // abort/cancel any request that we have pending when the component unmounts
+            controller.abort();
+        };
+    }, []);
+
+    return (
+        <article>
+            <h2>Users List</h2>
+            {users?.length ? (
+                <ul>
+                    {users.map((user, i) => (
+                        <li key={i}>{user?.username}</li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No users to display</p>
+            )}
+            <br />
+        </article>
+    );
+};
+
+export default Users;
---
+++ src/hooks/useRefreshToken.js
@@ -0,0 +1,26 @@
+import React from "react";
+import axios from "../api/axios";
+import useAuth from "./useAuth";
+
+const useRefreshToken = () => {
+    const { setAuth } = useAuth();
+
+    const refresh = async () => {
+        const response = await axios.get("/refresh", {
+            // settings that aalows us to send cookies
+            // with our request, and this request is going
+            // to send along our cookie that has the reponse token
+            withCredentials: true,
+        });
+        setAuth((prev) => {
+            console.log(JSON.stringify(prev));
+            console.log(response.data.accessToken);
+            return { ...prev, accessToken: response.data.accessToken };
+        });
+        return response.data.accessToken;
+    };
+
+    return refresh;
+};
+
+export default useRefreshToken;
